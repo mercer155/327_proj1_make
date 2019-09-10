840b472a5e85b593d8b6f92a20203b6596906fe0
Mitchell Paluzzi <paluzzi.mp@gmail.com>
2019-09-10 16:53:16 -0400

    done

---
+++ myfunc.cpp
@@ -0,0 +1,9 @@
+#include "myfunc.h"     //iostream is where std::cout and std::endl are defined
+
+std::string func()
+{
+        return "hello";
+}
+
+
+
---
+++ myfunc.h
@@ -0,0 +1,8 @@
+#include <iostream>     //iostream is where std::cout and std::endl are defined
+#include <string>
+#ifndef MYFUNC_H_
+#define MYFUNC_H_
+
+std::string func();
+
+#endif
